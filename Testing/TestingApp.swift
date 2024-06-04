b2431abbf804b070873bfefc259249d26e928bfd
Kasheeram <kashee.nitd2016@gmail.com>
2024-06-04 12:52:42 +0530

    Initial Commit

---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by kashee on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
